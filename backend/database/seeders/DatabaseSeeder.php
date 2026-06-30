716e9a5da9d8fcc3605e4fd917d4fc28c8cdc5ce
Herrison Trugilho <herrison.trugilho2@gmail.com>
2026-06-30 08:42:38 -0300

    feat: adicionando seeder de serviços

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             FuncoesUsuariosSeeder::class,
             UsuarioAdministradorSeeder::class,
             UsuarioAtendenteSeeder::class,
+            ServiceSeeder::class,
         ]);
     }
 }
